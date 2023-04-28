dec6f55f7120cb31fe6a3ee91248beb0688dc20e
ValentinGorovik <71607683+ValentinGorovik@users.noreply.github.com>
2023-04-28 10:02:32 +0300

    Delete repos - Copy/task directory

--- repos - Copy/task/task/generate_config.py
+++
@@ -1,5 +0,0 @@
-import configparser
-import pandas as pd
-config_file = configparser.ConfigParser()
-start_date=pd.to_datetime(input('Enter Start Date in "YYYY-MM-DD" format: ' ),format='%Y-%m-%d %H:%M:%S')
-last_date=pd.to_datetime(input('Enter Start Date in "YYYY-MM-DD" format: ' ),format='%Y-%m-%d %H:%M:%S')
